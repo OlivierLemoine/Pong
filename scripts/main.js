f0dc5845c92ae73df0a359e1fbab60c8483723d9
morillon fançois <francois.morillon@yahoo.fr>
2018-02-19 22:48:45 +0100

    HardMath

--- scripts/main.js
+++ scripts/main.js
@@ -9,24 +9,31 @@ var gm = new GameManager((a, b, point) => {
         y: oB.y - oA.y
     };
 
-    vect.x /= magnitude(vect);
-    vect.y /= magnitude(vect);
-
-    if(b.speed !== undefined){
-        b.speed.x *= vect.x;
-        b.speed.y *= vect.y;
-    }
+    let magVect = magnitude(vect)
+    vect.x /= magVect;
+    vect.y /= magVect;
 
     if(a.speed !== undefined){
-        a.speed.x *= -vect.x;
-        a.speed.y *= -vect.y;
+        let rot = scalairproduct(vect,a.speed)/magnitude(a.speed);
+        rot = Math.acos(rot);
+        let vectCircu = cartesianToCircular(a.speed);
+        vectCircu.t += Math.PI - rot;
+        a.speed = circularToCartesian(vectCircu);
+    }
+
+    if(b.speed !== undefined){
+        let rot = scalairproduct(vect,b.speed)/magnitude(b.speed);
+        rot = Math.acos(rot);
+        let vectCircu = cartesianToCircular(b.speed);
+        vectCircu.t += Math.PI - rot;
+        b.speed = circularToCartesian(vectCircu);
     }
 });
 gm.addStatic(new Wall("black", {x: 20, y: 1000}, {x: 0, y: 0}).display(g));
 gm.addStatic(new Wall("black", {x: 1000, y: 20}, {x: 0, y: 0}).display(g));
 gm.addStatic(new Wall("black", {x: 20, y: 1000}, {x: 1000, y: 0}).display(g));
 gm.addStatic(new Wall("black", {x: 1000, y: 20}, {x: 0, y: 1000}).display(g));
-gm.addMobile(new Rectangle("green", {x: 100, y: 100}, {x: 100, y: 100}, 0, {x: 0, y: 2}).display(g));
+gm.addMobile(new Rectangle("green", {x: 100, y: 100}, {x: 100, y: 100}, 0, {x: 1, y: 1}).display(g));
 // gm.addMobile(new Rectangle("black", {x: 100, y: 100}, {x: 50, y: 50}, 30).display(g));
 gm.launch();
 
